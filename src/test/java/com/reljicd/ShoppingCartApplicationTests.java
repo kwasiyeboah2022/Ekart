875368eacf772db2449a3af3f0ae17eb14d9d02d
kwasiyeboah2022 <121293145+kwasiyeboah2022@users.noreply.github.com>
2025-06-27 15:40:46 -0700

    Update ShoppingCartApplicationTests.java

--- src/test/java/com/reljicd/ShoppingCartApplicationTests.java
+++ src/test/java/com/reljicd/ShoppingCartApplicationTests.java
@@ -5,12 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ShoppingCartApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }
